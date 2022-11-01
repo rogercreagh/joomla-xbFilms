52aa9caeb1c8fc858e8cb6de993e94ca6eecec5b
rogercreagh <44980188+rogercreagh@users.noreply.github.com>
2022-11-01 21:08:27 +0000

    set values for camfmt, aspectratio

--- src/com_xbfilms/admin/sql/updates/mysql/0.9.9.9.sql
+++ src/com_xbfilms/admin/sql/updates/mysql/0.9.9.9.sql
@@ -1,5 +1,5 @@
 # v0.9.9.9 insert tech_notes
 ALTER TABLE `#__xbfilms` ADD `tech_notes` VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'added v0.9.9.9' AFTER `filmsound`;
-#merge cam_format and filmsound into tech_notes
-UPDATE `#__xbfilms` SET `tech_notes` =  CONCAT('Camera : ',`cam_format`,'. ') WHERE`cam_format <> '';
-UPDATE `#__xbfilms` SET `tech_notes` =  CONCAT(`tech_notes`,`filmsound`) WHERE`filmsound <> '';
+# merge cam_format and filmsound into tech_notes
+UPDATE `#__xbfilms` SET `tech_notes` =  CONCAT('Camera : ',`cam_format`,'. ') WHERE `cam_format` != '';
+UPDATE `#__xbfilms` SET `tech_notes` =  CONCAT(`tech_notes`,`filmsound`) WHERE `filmsound` != '';
