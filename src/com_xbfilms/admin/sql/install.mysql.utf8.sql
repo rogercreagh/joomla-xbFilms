2fa211a333f338f1582acf44a652cd0603cd7f4f
rogercreagh <44980188+rogercreagh@users.noreply.github.com>
2022-12-31 16:24:25 +0000

    v1.0.1.0 add table #__xbfilmgroup

--- src/com_xbfilms/admin/sql/install.mysql.utf8.sql
+++ src/com_xbfilms/admin/sql/install.mysql.utf8.sql
@@ -1,4 +1,4 @@
-# sql installation file for component xbFilms v1.0.0.1 13th December 2022
+# sql installation file for component xbFilms v1.0.1.0 31st December 2022
 # NB no film data is installed with this file, default categories are created by the installation script
 
 INSERT INTO `#__content_types` (`type_title`, `type_alias`, `content_history_options`, `table`, `field_mappings`, `router`,`rules`) 
@@ -94,65 +94,6 @@ VALUES
 	"note":"note"}}',
 'XbfilmsHelperRoute::getCategoryRoute','');
 
-CREATE TABLE IF NOT EXISTS `#__xbpersons` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `asset_id` int(10) unsigned NOT NULL DEFAULT '0',
-  `firstname` varchar(190) NOT NULL DEFAULT '',
-  `lastname` varchar(190) NOT NULL DEFAULT '',
-  `alias` varchar(190) NOT NULL DEFAULT '',
-  `summary` varchar(255) NOT NULL DEFAULT '',
-  `biography` mediumtext,
-  `portrait` mediumtext NOT NULL DEFAULT '',
-  `nationality` varchar(100) NOT NULL DEFAULT '',
-  `year_born` smallint,
-  `year_died` smallint,
-  `ext_links` mediumtext,
-  `catid` int(10) NOT NULL  DEFAULT '0',
-  `access` int(10) NOT NULL  DEFAULT '0',
-  `state` tinyint(3) NOT NULL DEFAULT '0',
-  `created` datetime,
-  `created_by` int(10) NOT NULL DEFAULT '0',
-  `created_by_alias` varchar(255) NOT NULL DEFAULT '',
-  `checked_out` int(10) unsigned NOT NULL DEFAULT '0',
-  `checked_out_time` datetime,
-  `modified` datetime,
-  `modified_by` int(10) NOT NULL DEFAULT '0',
-  `metadata` mediumtext NOT NULL DEFAULT '',
-  `ordering` int(10) NOT NULL DEFAULT '0',
-  `params` mediumtext NOT NULL DEFAULT '',
-  `note` mediumtext,
-  PRIMARY KEY (`id`)
-)  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
-
-# CREATE UNIQUE INDEX `personaliasindex` ON `#__xbpersons` (`alias`);
-
-CREATE TABLE IF NOT EXISTS `#__xbcharacters` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `asset_id` int(10) unsigned NOT NULL DEFAULT '0',
-  `name` varchar(100) NOT NULL DEFAULT '',
-  `alias` varchar(190) NOT NULL DEFAULT '',
-  `summary` varchar(255) NOT NULL DEFAULT '',
-  `description` mediumtext,
-  `image` mediumtext NOT NULL DEFAULT '',
-  `catid` int(10) NOT NULL  DEFAULT '0',
-  `access` int(10) NOT NULL  DEFAULT '0',
-  `state` tinyint(3) NOT NULL DEFAULT '0',
-  `created` datetime,
-  `created_by` int(10) NOT NULL DEFAULT '0',
-  `created_by_alias` varchar(255) NOT NULL DEFAULT '',
-  `checked_out` int(10) unsigned NOT NULL DEFAULT '0',
-  `checked_out_time` datetime,
-  `modified` datetime,
-  `modified_by` int(10) NOT NULL DEFAULT '0',
-  `metadata` mediumtext NOT NULL DEFAULT '',
-  `ordering` int(10) NOT NULL DEFAULT '0',
-  `params` mediumtext NOT NULL DEFAULT '',
-  `note` mediumtext,
-  PRIMARY KEY (`id`)
-)  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
-
-# CREATE UNIQUE INDEX `characteraliasindex` ON `#__xbcharacters` (`alias`);
-
 CREATE TABLE IF NOT EXISTS `#__xbfilms` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `asset_id` int(10) unsigned NOT NULL DEFAULT '0',
@@ -209,6 +150,18 @@ CREATE TABLE IF NOT EXISTS `#__xbfilmperson` (
   KEY `idx_film_id` (`film_id`),
   KEY `idx_person_id` (`person_id`),
   KEY `idx_role` (`role`)
+)  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `#__xbfilmgroup` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `film_id` int(11) NOT NULL DEFAULT '0',
+  `group_id` int(11) NOT NULL DEFAULT '0',
+  `role` varchar(255) NOT NULL DEFAULT '',
+  `role_note` varchar(255) NOT NULL DEFAULT '',
+  `listorder` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `idx_film_id` (`film_id`),
+  KEY `idx_group_id` (`group_id`)
 )  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `#__xbfilmcharacter` (
